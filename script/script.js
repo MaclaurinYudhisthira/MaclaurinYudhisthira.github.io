3e336e5643b87dc4ef90ec14edb80334b21bd500
kishan Dasondhi <kishandasondhi123@gmail.com>
2019-06-17 12:59:13 +0530

    edited animation

--- script/script.js
+++ script/script.js
@@ -2,13 +2,11 @@ let up=0,pos=0;
 const gif=document.getElementById('gift');
 if(innerWidth<=480)
 {
-    gif.addEventListener('click',myMove1)
-    gif.style.color="red";
+    gif.addEventListener('click',myMove1) 
 }
 else
 {
     gif.addEventListener('click',myMove);
-    gif.style.color="yellow";
 }
 
 function myMove() {
@@ -41,7 +39,32 @@ function myMove() {
     }
 }
 
-function myMove1()
-{
-    console.log('df');
+function myMove1() {
+    var elem = document.querySelector('.container'); 
+    if(up==0)
+    {
+        var id = setInterval(frame, 5);
+        up=1;
+    }
+    else
+    {
+        var id = setInterval(dframe, 5);
+        up=0;
+    }
+    function frame() {
+        if (pos == -176) {
+            clearInterval(id);
+        } else {
+            pos--; 
+            elem.style.marginTop = `${pos}px`; 
+        }
+    }
+    function dframe() {
+        if (pos == 0) {
+            clearInterval(id);
+        } else {
+            pos++; 
+            elem.style.marginTop = `${pos}px`; 
+        }
+    }
 }
